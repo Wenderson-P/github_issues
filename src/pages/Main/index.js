81977b507e90cde5117de52f69b32f952ff11449
Wenderson <wps713@gmail.com>
2019-11-01 22:22:35 -0300

    Main page state and form actions added

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,16 +5,35 @@ import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
 export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.newRepo);
+  };
+
   render() {
+    const { newRepo } = this.state;
     return (
       <Container>
         <h1>
           <FaGithubAlt />
           Repositorios
         </h1>
-        <Form onSubmit={() => {}}>
-          <input type="text" placeholder="Adicionar repositório" />
-          <SubmitButton disabled>
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton>
             <FaPlus color="#FFF" size={14} />
           </SubmitButton>
         </Form>
