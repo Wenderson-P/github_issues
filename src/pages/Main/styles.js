ef7ab3d0c88c51572348b2a8bf33ce0e58df6129
Wenderson <wps713@gmail.com>
2019-11-01 22:06:41 -0300

    Main page title style removed and added Container style css

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: ${props => (props.error ? 'red' : '#7159c1')};
-  font-family: Arial, Helvetica, sans-serif;
-  small {
-    font-size: 14px;
-    color: #333;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-right: 10px;
+    }
   }
 `;
+
+export const Form = styled.form``;
+
+export const SubmitButton = styled.button``;
