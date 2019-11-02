6dc81698f540a1cb7800e3692479f3db5163a260
Wenderson <wps713@gmail.com>
2019-11-01 22:17:58 -0300

    Main page changed to class

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositorios
-      </h1>
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  render() {
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositorios
+        </h1>
+        <Form onSubmit={() => {}}>
+          <input type="text" placeholder="Adicionar repositório" />
+          <SubmitButton disabled>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
-export default Main;
